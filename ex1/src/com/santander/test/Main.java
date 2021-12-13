4dba3d893740a429370fcf116115f224b8491e41
Welder <askowiks@gmail.com>
2021-12-12 23:21:46 -0300

    lista3

--- ex1/src/com/santander/test/Main.java
+++ ex1/src/com/santander/test/Main.java
@@ -1,8 +1,20 @@
 package com.santander.test;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write yur code here
+
+        String [] carrinho = new String[5];
+        Scanner entrada = new Scanner(System.in);
+
+        for (int indice = 0; indice <= carrinho.length - 1; indice++){
+            System.out.println("Escreva o nome da fruta para adicionar ao carrinho:");
+            carrinho[indice] = entrada.next();
+        }
+        for (int indice = 0; indice <= carrinho.length - 1; indice++){
+            System.out.print(" | " + carrinho[indice]);
+        }
     }
 }
--- ex2/src/com/santander/test/Main.java
+++ ex2/src/com/santander/test/Main.java
@@ -1,8 +1,19 @@
 package com.santander.test;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code
+
+        Scanner palavraScanner = new Scanner(System.in);
+        String palavra = palavraScanner.next();
+        String[] arrayPalavra = new String[palavra.length()];
+
+        for (int i = 0; i < palavra.length(); i++){
+        arrayPalavra[i] = palavra[i];
+        }
+
+
     }
 }
