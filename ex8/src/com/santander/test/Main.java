ebc7a2e7a1770efebc400c15c27997f03ba3bc3f
Welder <askowiks@gmail.com>
2021-12-13 16:27:08 -0300

    lista3

---
+++ ex8/src/com/santander/test/Main.java
@@ -0,0 +1,17 @@
+package com.santander.test;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        String nome = "";
+        Double peso = 0.0;
+        Double altura = 0.0;
+        
+
+    }
+}
